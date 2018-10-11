8ffa9a1e2ed9f930cd8220dbc39ab3d203ff0eb9
iiPlasma <ZIvy042003@gmail.com>
2018-10-10 20:47:24 -0400

    Docstrings

--- sequestrum/logging.py
+++ sequestrum/logging.py
@@ -1,36 +1,45 @@
 # Logging module
 
-import sys
-import time
-
-def delay_print(string):
-    for character in string:
-        sys.stdout.write(character)
-        sys.stdout.flush()
-        time.sleep(0.07)
-
-    print("\n", end="")
+from sys import exit
 
 
 def format_output(error_type, error_message):
-        return("[{}] {}".format(error_type, error_message))
+    """
+        Formats the errors to look standard
+    """
+    return "[{}] {}".format(error_type, error_message)
 
 
 def print_fatal(error_message):
+    """
+        Prints message with FATAL
+    """
     print("\033[1;31mFATAL\033[0m {}".format(error_message))
-    sys.exit()
+    exit()
 
 
 def print_error(error_message):
+    """
+        Prints message with ERROR
+    """
     print("\033[1;31mERROR\033[0m {}".format(error_message))
 
 
 def print_warn(error_message):
+    """
+        Prints message with WARN
+    """
     print("\033[1;33mWARN\033[0m {}".format(error_message))
 
 
 def print_info(error_message):
+    """
+        Prints message with INFO
+    """
     print("\033[1;32mINFO\033[0m {}".format(error_message))
 
 def print_verbose(error_message):
+    """
+        Prints message with green color to show success
+    """
     print("\033[1;32mVERBOSE\033[0m {}".format(error_message))
